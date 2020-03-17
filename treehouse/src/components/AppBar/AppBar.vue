c96671b87ad4b4cf8fec10913669289d9971746a
loney <geo.giatrakos@gmail.com>
2020-03-17 20:39:18 +0200

    add images

--- treehouse/src/components/AppBar/AppBar.vue
+++ treehouse/src/components/AppBar/AppBar.vue
@@ -9,7 +9,7 @@
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="../../assets/treehouse.png"
           transition="scale-transition"
           width="40"
         />
@@ -19,7 +19,7 @@
           class="shrink mt-1 hidden-sm-and-down"
           contain
           min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          src="../../assets/treehouse-name-dark.png"
           width="100"
         />
       </div>
