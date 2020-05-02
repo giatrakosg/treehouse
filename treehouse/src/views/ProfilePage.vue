0642f09bed7322d438d9aa16844cd6a9aaf696ad
loney <geo.giatrakos@gmail.com>
2020-05-02 18:24:07 +0300

    add profilecard

--- treehouse/src/views/ProfilePage.vue
+++ treehouse/src/views/ProfilePage.vue
@@ -1,12 +1,22 @@
 <template lang="html">
-  <div class="">
+    <v-layout row>
+        <v-flex sm1 md1 lg1>
+            <v-spacer></v-spacer>
+        </v-flex>
+        <v-flex sm10 md10 lg10 >
+            <ProfileInfoCard />
+        </v-flex>
+    </v-layout>
 
-  </div>
 </template>
 
 <script>
+import ProfileInfoCard from "../components/Profile/ProfileInfoCard.vue"
 export default {
-    name : 'ProfilePage'
+    name : 'ProfilePage' ,
+    components : {
+        ProfileInfoCard
+    }
 }
 </script>
 
