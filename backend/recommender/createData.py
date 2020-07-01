8db1e5ac0152c42571e253aad6c5e0ebeb97aac0
loney <geo.giatrakos@gmail.com>
2020-07-01 18:43:29 +0300

    implement createData
    + Implement create rooms

--- backend/recommender/createData.py
+++ backend/recommender/createData.py
@@ -4,11 +4,13 @@ sys.path.append('../')
 from database import db
 from models.User import User
 from models.Review import Review
-from models.Room import Room
+from models.Room import Room, RoomTypes
 from routes.Users import generate_password_hash
 import pandas as pd
 import random
 import uuid
+import re
+import numpy as np
 from tqdm import tqdm
 
 def createUser(seen,rec):
@@ -22,18 +24,59 @@ def createUser(seen,rec):
     seen.add(rec['reviewer_id'])
 
 def createRoom(rec):
-    room = Room()
+    if rec['room_type'] == 'Entire home/apt':
+        rt = RoomTypes.house
+    if rec['room_type'] == 'Private room':
+        rt = RoomTypes.private_room
+    if rec['room_type'] == 'Shared room':
+        rt = RoomTypes.shared_room
+
+
+    hosts = User.query.filter_by(isHost=True).all()
+    ind = random.randrange(len(hosts))
+    host = hosts[ind]
+
+    lounge = bool(random.getrandbits(1))
+    smoking = bool(random.getrandbits(1))
+    pets = bool(random.getrandbits(1))
+    events = bool(random.getrandbits(1))
+    wifi = bool(random.getrandbits(1))
+    ac = bool(random.getrandbits(1))
+    refr = bool(random.getrandbits(1))
+    kitch = bool(random.getrandbits(1))
+    tv = bool(random.getrandbits(1))
+    parking = bool(random.getrandbits(1))
+    elev = bool(random.getrandbits(1))
+    area = random.randrange(150)
+
+
+    room = Room(rt=rt,beds_num=rec['beds'],baths_num=rec['bathrooms'],bedr_num=rec['bedrooms'],
+    lounge=lounge,desc=rec['description'],smoking_all=smoking,pets_all=pets,events_all=events,
+    wifi=wifi,ac=ac,refrigerator=refr,kitchen=kitch,tv=tv,parking=parking,elevator=elev,latitude=rec['latitude'],
+    longitude=rec['longitude'],address=rec['street']+','+rec['city'],tran_info=rec['transit'],pers_num=rec['guests_included'],
+    standard_cost=rec['price'],add_prs_cost=3,title=rec['name'],area=area,min_stay=rec['minimum_nights'],owner_id=host.id
+    )
+    db.session.add(room)
 
 revpd = pd.read_csv('../data/reviews_less.csv',index_col=0)
 roompd = pd.read_csv('../data/listings.csv',index_col='id')
 
-tqdm.pandas()
+# We remove the . thousand(separator) and the first char that is the dollar sign
+roompd['price'] = roompd['price'].apply(lambda x: x.split()[0].replace(',', ''))
+roompd['price'] = roompd['price'].str[1:].astype(np.float32)
+roompd['guests_included'] = roompd['guests_included'].astype(np.int16)
 
-print(revpd.head())
+tqdm.pandas()
 
 seen = set([])
 
 #for index, row in tqdm(revpd.iterrows(),total=revpd.shape[0]):
 #    createUser(seen,row)
+roompd['bathrooms'].fillna(0,inplace=True)
+roompd['beds'].fillna(0,inplace=True)
+roompd['bedrooms'].fillna(0,inplace=True)
+
+roompd.progress_apply(lambda x : createRoom(x),axis=1)
 revpd.progress_apply(lambda x : createUser(seen,x),axis=1)
+
 db.session.commit()
