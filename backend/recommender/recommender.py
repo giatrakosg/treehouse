5e6e95444e126e4374cb70439eabb5676e4a1efa
loney <geo.giatrakos@gmail.com>
2020-07-02 23:52:17 +0300

    Generate recommendations

--- backend/recommender/recommender.py
+++ backend/recommender/recommender.py
@@ -5,11 +5,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from ExplicitMF import Recommender, train_test_split
+from tqdm import tqdm
 
 from database import db
 from models.User import User
 from models.Room import Room
-from models.Recommendation import
+from models.Recommendation import Recommendation
 
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
@@ -30,17 +31,25 @@ reviews['iidx'] = reviews['listing_id'].apply(lambda x: np.where(items==x)[0][0]
 for idx,row in reviews.iterrows():
     ratings[row[7],row[8]] = row[6]
 
-
 mf = Recommender(n_epochs=5)
-
 train, val = train_test_split(ratings)
-
 mf.fit(train,val)
 
-print('Predicting for user 10')
-print(mf.predict(train,10))
+renters = User.query.filter_by(isHost=False,isAdmin=False).all()
+rentidx = 0
 
-#for row in reviews.itertuples():
-    #print(row)
-#    ratings[row[4]-1, row[1]-1] = row[7]
-#print(ratings)
+for u in tqdm(range(253)):
+    rat_u = mf.predict(train,u)
+    top_5 = rat_u.argsort()[0:5]
+    renterid = renters[rentidx].id
+    rentidx+=1
+
+    for num,ind in enumerate(top_5):
+        idx = (reviews['iidx'] == ind).idxmax()
+        listingid = reviews.iloc[idx]['listing_id']
+        room = Room.query.filter_by(listingid=int(listingid)).first()
+        if room:
+            rec = Recommendation(rank=num,uid=renterid,roomid=room.id)
+            db.session.add(rec)
+
+db.session.commit()
